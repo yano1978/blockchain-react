141d25c99525454dbfd26c39e64b46dd1f8f33d2
yano1978 <yano1978@gmail.com>
2021-05-15 12:24:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blockchain-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/blockchain-react)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './style.css';
+import Web3 from 'web3';
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockChainData();
+  }
+  async loadBlockChainData() {
+    const web3 = new Web3(Web3.givenProvider);
+    const network = await web3.eth.net.getNetworkType();
+    console.log('network:', network);
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
