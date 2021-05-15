77a0116dc888bc19af3c3aa710e8a60c1ed2b602
ernestoianuario <yano1978@gmail.com>
2021-05-15 13:21:07 +0100

    App connected with Etherum account succesfully

--- src/App.js
+++ src/App.js
@@ -9,12 +9,26 @@ class App extends Component {
   async loadBlockChainData() {
     const web3 = new Web3(Web3.givenProvider || "localhost://8545");
     const network = await web3.eth.net.getNetworkType();
-    console.log('network:', network);
+    console.log('network: ', network);
+    window.ethereum.enable().then((account) =>{
+      const defaultAccount = account[0]
+      web3.eth.defaultAccount = defaultAccount
+      this.setState({"account": defaultAccount})
+      console.log("account: ", defaultAccount);
+    })
+  }
+  // Fetch Account
+  constructor(props){
+    super(props);
+    this.state = {
+      account: ''
+    }
   }
   render() {
     return (
       <div className="container">
         <h1>Hello world</h1>
+        <p>Your account: {this.state.account}</p>
       </div>
     );
   }
